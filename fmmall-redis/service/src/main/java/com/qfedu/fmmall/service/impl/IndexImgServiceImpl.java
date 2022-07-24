795c5389d40abbc2163b46948f351a03f790aabb
shishen <crh8023@outlook.com>
2022-07-24 15:43:28 +0800

    首页轮播图查询基于redis

--- fmmall-redis/service/src/main/java/com/qfedu/fmmall/service/impl/IndexImgServiceImpl.java
+++ fmmall-redis/service/src/main/java/com/qfedu/fmmall/service/impl/IndexImgServiceImpl.java
@@ -1,24 +1,52 @@
 package com.qfedu.fmmall.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qfedu.fmmall.dao.IndexImgMapper;
 import com.qfedu.fmmall.entity.IndexImg;
+import com.qfedu.fmmall.entity.ProductImg;
+import com.qfedu.fmmall.entity.ProductSku;
 import com.qfedu.fmmall.service.IndexImgService;
 import com.qfedu.fmmall.vo.ResStatus;
 import com.qfedu.fmmall.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class IndexImgServiceImpl implements IndexImgService {
 
     @Autowired
     private IndexImgMapper indexImgMapper;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+    private ObjectMapper objectMapper=new ObjectMapper();
 
     @Override
     public ResultVO listIndexImgs() {
-        List<IndexImg> indexImgs = indexImgMapper.listIndexImgs();
+        List<IndexImg> indexImgs=null;
+        try {
+            //String结构缓存轮播图信息
+            //String indexImgs1 = stringRedisTemplate.opsForValue().get("indexImgs");
+            String imgsStr = stringRedisTemplate.boundValueOps("indexImgs").get();
+            if(imgsStr!=null){
+                JavaType javaType1=objectMapper.getTypeFactory().constructParametricType(ArrayList.class, IndexImg.class);
+                indexImgs=objectMapper.readValue(imgsStr,javaType1);
+            }else{
+                indexImgs = indexImgMapper.listIndexImgs();
+                stringRedisTemplate.boundValueOps("indexImgs").set(objectMapper.writeValueAsString(indexImgs));
+                //设置过期时间为1天
+                stringRedisTemplate.boundValueOps("indexImgs").expire(1, TimeUnit.DAYS);
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         if(indexImgs.size()==0){
             return new ResultVO(ResStatus.NO,"fail",null);
         }else {
